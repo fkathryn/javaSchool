d5c8e31f4ee87c30a259bb0da392c3e7393b2c90
Alina Sokolova <fkathryn@student.21-school.ru>
2021-04-15 19:56:44 +0300

    MyArrayList with Comparator<Integer>

---
+++ myArrayList/src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+
+    public static boolean compare(int first, int second) {
+        return first > second;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("🌝 int");
+        MyArrayList<Integer> list = new MyArrayList<>(10);
+        System.out.println("Size = " + list.getSize());
+        list.set(0, 33);
+        list.set(1, 323);
+        list.set(2, 3);
+        list.set(3, 313);
+        list.set(4, 133);
+        list.set(5, 332);
+        list.set(6, 13);
+        list.set(7, 31);
+        list.set(8, 331);
+        list.set(9, 133);
+
+        for (int i = 0; i < list.getSize(); i++)
+            System.out.println("Index = " + i + " : " + list.get(i));
+
+        System.out.println("*** delete ***");
+        list.delete(5);
+        for (int i = 0; i < list.getSize(); i++)
+            System.out.println("Index = " + i + " : " + list.get(i));
+
+        System.out.println("*** sort ***");
+        list.sort(new MyComparatorInteger());
+        for (int i = 0; i < list.getSize(); i++)
+            System.out.println("Index = " + i + " : " + list.get(i));
+        System.out.println(list.iterator().next());
+        System.out.println(list.iterator().next());
+        System.out.println(list.iterator().next());
+        System.out.println(list.iterator().next());
+
+
+        System.out.println("🌝 String");
+        MyArrayList<String> list1 = new MyArrayList<>(10);
+        System.out.println("Size = " + list1.getSize());
+        list1.set(0, "Users");
+        list1.set(1, "qwerty");
+        list1.set(2, "Hello");
+        list1.set(3, "hello!");
+        list1.set(4, "fkathryn");
+        list1.set(5, "Library");
+        list1.set(6, "VirtualMachines");
+        list1.set(7, "Contents");
+        list1.set(8, "Home");
+        list1.set(9, "java");
+
+        for (int i = 0; i < list1.getSize(); i++)
+            System.out.println("Index = " + i + " : " + list1.get(i));
+
+        System.out.println("*** delete ***");
+        list1.delete(6);
+        for (int i = 0; i < list1.getSize(); i++)
+            System.out.println("Index = " + i + " : " + list1.get(i));
+    }
+}
---
+++ myArrayList/src/MyArrayList.java
@@ -0,0 +1,138 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyArrayList<T> implements Iterable<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int size;
+    private int capacity;
+    private T[] array;
+    private int pointer = 0;
+
+    public MyArrayList() {
+        this.size = 0;
+        this.capacity = DEFAULT_CAPACITY;
+        this.array = (T[]) new Object[capacity];
+    }
+
+    public MyArrayList(int size) {
+        this.size = size;
+        this.capacity = size * 2;
+        this.array = (T[]) new Object[capacity];
+    }
+
+    public MyArrayList(T[] array) {
+        this.size = array.length;
+        this.capacity = array.length * 2;
+        this.array = (T[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            array[i] = this.array[i];
+        }
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public T get(int index) {
+        if (checkIndexOutOfBound(index)) {
+            throw new IndexOutOfBoundsException("Index out of bound");
+        }
+        return this.array[index];
+    }
+
+    public void set(int index, T element) {
+        if (checkIndexOutOfBound(index)) {
+            throw new IndexOutOfBoundsException("Index out of bound");
+        } else {
+            this.array[index] = element;
+        }
+    }
+
+    public void delete(int index) {
+        if (checkIndexOutOfBound(index)) {
+            throw new IndexOutOfBoundsException("Index out of bound");
+        } else {
+            for (int i = index; i < size - 1; i++) {
+                this.array[i] = this.array[i + 1];
+            }
+        }
+        this.size -= 1;
+    }
+
+    public void sort(Comparator comparator) {
+        quickSortArrays(this.array, 0, size - 1, comparator);
+    }
+
+    private void quickSortArrays(T[] array, int begin, int end, Comparator comparator) {
+        if (end <= begin) {
+            return;
+        }
+        int position = partition(array, begin, end, comparator);
+        quickSortArrays(array, begin, position - 1, comparator);
+        quickSortArrays(array, position + 1, end, comparator);
+    }
+
+    private int partition(T[] array, int begin, int end, Comparator comparator) {
+        int counter = begin;
+        for (int i = begin; i <= end; i++) {
+            if (comparator.compare(array[i], array[end]) <= 0) {
+                T temp = array[counter];
+                array[counter] = array[i];
+                array[i] = temp;
+                counter++;
+            }
+        }
+        return counter - 1;
+    }
+
+    public T[] toArray() {
+        T[] newArray = (T[]) new Object[this.size];
+        for (int i = 0; i < this.size; i++) {
+            newArray[i] = this.array[i];
+        }
+        return newArray;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof MyArrayList) {
+            MyArrayList array = (MyArrayList) obj;
+            if (this.size != array.size) {
+                return false;
+            } else {
+                for (int i = 0; i < this.size; i++) {
+                    if (this.array[i] != array.array[i]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int i = 0;
+            @Override
+            public boolean hasNext() {
+                return i < size - 1;
+            }
+
+            @Override
+            public T next() {
+                if (hasNext()) {
+                    return get(i++);
+                } else {
+                    throw new NoSuchElementException("No element" + size);
+                }
+            }
+        };
+    }
+    private boolean checkIndexOutOfBound(int index) {
+        return (index < 0 || index > size - 1);
+    }
+}
